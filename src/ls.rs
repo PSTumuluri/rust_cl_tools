07b83261538b3c063e3a308563558efc3c29c9b9
Pavan Tumuluri <pst15635@uga.edu>
2022-08-01 15:14:49 -0400

    cmd-ls: supports listing individual regular files

--- src/ls.rs
+++ src/ls.rs
@@ -2,10 +2,11 @@ use std::env;
 use std::error::Error;
 use std::io;
 use std::fs::{self, DirEntry, ReadDir};
+use std::path::{Path, PathBuf};
 
 /// Represents the configuration for this command.
 struct Config {
-    dir_path_vec: Vec<String>,
+    path_vec: Vec<PathBuf>,
     long_list: bool,
     list_all: bool,
 }
@@ -14,7 +15,7 @@ impl Config {
     /// Constructs a default config object where all options are off.
     fn from_default() -> Config {
         Config {
-            dir_path_vec: vec![],
+            path_vec: vec![],
             long_list: false,
             list_all: false,
         }
@@ -26,9 +27,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let args: Vec<String> = env::args().collect();
     let config = parse_config(&args)?;
-    for dir_path in &config.dir_path_vec {
-        if let Err(_) = process_dir_path(dir_path, &config) {
-            println!("Directory not found: {}", dir_path);
+    for path in &config.path_vec {
+        if let Err(_) = process_path(path, &config) {
+                println!("Directory not found: {}", path.display());
         }
     }
 
@@ -53,12 +54,12 @@ fn parse_config(args: &[String]) -> Result<Config, &str> {
                 }
             }
         } else {
-            config.dir_path_vec.push(String::from(arg));
+            config.path_vec.push(PathBuf::from(arg));
         }
     }
 
-    if config.dir_path_vec.is_empty() {
-        config.dir_path_vec.push(String::from("."));
+    if config.path_vec.is_empty() {
+        config.path_vec.push(PathBuf::from("."));
     }
 
     Ok(config)
@@ -67,11 +68,17 @@ fn parse_config(args: &[String]) -> Result<Config, &str> {
 /// Visits the specified path, printing its information if a file, or printing its 
 /// contents' information if a directory.
 /// Returns an error if the path does not correspond to an existing directory.
-fn process_dir_path(dir_path: &String, config: &Config) -> io::Result<()> {
-    let dir_iter = fs::read_dir(dir_path)?;
-    for entry in dir_iter {
-        let entry = entry?;
-        process_entry(&entry, config);
+fn process_path(path: &Path, config: &Config) -> io::Result<()> {
+    if path.is_file() {
+        process_entry(path, config);
+    } else if path.is_dir() {
+        let dir_iter = path.read_dir()?;
+        for entry in dir_iter {
+            let entry = entry?;
+            process_entry(&entry.path(), config);
+        }
+    } else {
+        println!("File type not supported for {}", path.display());
     }
 
     Ok(())
@@ -80,11 +87,18 @@ fn process_dir_path(dir_path: &String, config: &Config) -> io::Result<()> {
 /// Displays a directory entry according the configured settings.
 /// Panics when the OsString cannot be converted to a UTF-8 string because I don't know
 /// what else to do.
-fn process_entry(entry: &DirEntry, config: &Config) {
-    let entry_string = entry.file_name().into_string().unwrap();
-    if config.list_all || (!config.list_all && entry_string.as_bytes()[0] != b'.') {
-        println!("{}", entry_string);
+fn process_entry(path: &Path, config: &Config) {
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+    if !config.list_all && file_name.as_bytes()[0] == b'.' {
+        return;
     }
+    if config.long_list {
+        print_long_list(path);
+    }
+    println!("{}", file_name);
+}
+
+fn print_long_list(path: &Path) {
 }
 
 #[cfg(test)]
@@ -101,12 +115,12 @@ mod tests {
         assert!(config.long_list);
         assert!(config.list_all);
 
-        let dir_path_vec = config.dir_path_vec;
-        assert!(dir_path_vec.contains(&String::from(".")));
-        assert!(dir_path_vec.contains(&String::from("..")));
-        assert!(dir_path_vec.contains(&String::from("~")));
+        let path_vec = config.path_vec;
+        assert!(path_vec.contains(&String::from(".")));
+        assert!(path_vec.contains(&String::from("..")));
+        assert!(path_vec.contains(&String::from("~")));
         
-        assert!(!dir_path_vec.contains(&String::from("-l")));
-        assert!(!dir_path_vec.contains(&String::from("-a")));
+        assert!(!path_vec.contains(&String::from("-l")));
+        assert!(!path_vec.contains(&String::from("-a")));
     }
 }
